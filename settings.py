db8db4d8d2accc2c13eaf75d10667aea7d6e15b2
Karimotoo <patrykd312@gmail.com>
2021-04-19 18:26:14 +0200

    fix: adding config file

---
+++ settings.py
@@ -0,0 +1,13 @@
+# SETTINGS FILE
+# TOKEN - discord app token
+# BOT PREFIX - no explanation needed (uhh I think)
+# API_AUTH - hypixel api auth
+# DB_CLIENT - your DB URI
+# DB_NAME - no explanation needed 
+# APPLICATION_ID - discord app id
+TOKEN=''
+BOT_PREFIX='$'
+API_AUTH=''
+DB_CLIENT = ''
+DB_NAME = ''
+APPLICATION_ID=''
