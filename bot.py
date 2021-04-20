91da94808680530332c985a0bf8808b7ea5900cc
Karimotoo <patrykd312@gmail.com>
2021-04-20 13:53:01 +0200

    feat: bot status

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import discord
-import os, sys
+import os
+import sys
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 from database import Database
 if not os.path.isfile("settings.py"):
-	sys.exit("'settings.py' not found!")
+    sys.exit("'settings.py' not found!")
 else:
-	import settings
+    import settings
 import session
 import logging
 
@@ -15,22 +16,22 @@ import logging
 bot = Bot(command_prefix=settings.BOT_PREFIX)
 
 logging.basicConfig(filename='./logs/log',
-                            filemode='a',
-                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.INFO)
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
 
 @bot.event
 async def on_ready():
-  print('Logged as {0.user}'.format(bot))
-  Database.initialize()
-  session.WebSession.create()
-  print(bot.guilds)
-
-
-async def status_task():
-    while True:
-        await bot.change_presence(activity=discord.Game("I love you"))
+    print('Logged as {0.user}'.format(bot))
+    Database.initialize()
+    session.WebSession.create()
+    print(bot.guilds)
+    # activityvar = discord.Activity(
+    #     type=discord.ActivityType.custom, state=f"{settings.BOT_PREFIX}help")
+    # await bot.change_presence(activity=activityvar)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{settings.BOT_PREFIX}help"))
 
 if __name__ == "__main__":
     for file in os.listdir("./cogs"):
@@ -50,7 +51,8 @@ async def on_command_completion(ctx):
     split = fullCommandName.split(" ")
     executedCommand = str(split[0])
     print(f"Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
-    logging.info(f"Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+    logging.info(
+        f"Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
 
 
 @bot.event
@@ -59,28 +61,35 @@ async def on_command_error(ctx, error):
     executedCommand = str(ctx.command)
 
     if isinstance(error, commands.CommandOnCooldown):
-        em = discord.Embed(title=f"Slow it down bro!",description=f"Try again in {error.retry_after:.2f}s.")
+        em = discord.Embed(title=f"Slow it down bro!",
+                           description=f"Try again in {error.retry_after:.2f}s.")
         await ctx.send(embed=em)
-        logging.info(f"[On cooldown] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+        logging.info(
+            f"[On cooldown] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(f"Missing argument")
-        logging.info(f"[Missing argument] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+        logging.info(
+            f"[Missing argument] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
 
     elif isinstance(error, commands.BadArgument):
         await ctx.send("Bad argument!")
-        logging.info(f"[Bad argument] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+        logging.info(
+            f"[Bad argument] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
 
     elif isinstance(error, commands.errors.CheckFailure):
-        await ctx.send( "Error: {}".format(error.param))
-        logging.warning(f"[Checkfailure] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+        await ctx.send("Error: {}".format(error.param))
+        logging.warning(
+            f"[Checkfailure] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
 
     elif isinstance(error, commands.errors.ExpectedClosingQuoteError):
         await ctx.send(f"Quote bug")
-        logging.info(f"[Quote not closed] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+        logging.info(
+            f"[Quote not closed] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
 
-    elif isinstance(error,commands.errors.CommandNotFound):
+    elif isinstance(error, commands.errors.CommandNotFound):
         await ctx.send(f"{error}")
-        logging.info(f"[Tried weird command] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+        logging.info(
+            f"[Tried weird command] Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
     raise error
 
 
