538811adee6b689bcc2b580eb21e91a10ff7c7c6
Karimotoo <patrykd312@gmail.com>
2021-04-21 14:31:57 +0200

    fix: fixed cata calculation

--- cogs/stat.py
+++ cogs/stat.py
@@ -45,7 +45,6 @@ class Stat(commands.Cog, name="stat"):
         embed3 = await self.embed_slayers_top3(arg)
         embed4 = await self.embed_dungeons_fastest_time_top3(arg)
         
-
         embeds = [embed,embed1,embed2,embed3,embed4]
         await message.edit(embed=embed) #stop "collecting data"
 
@@ -360,8 +359,6 @@ class Stat(commands.Cog, name="stat"):
         return top3avg
 
 
-
-
     #that's a long name ngl
     async def get_skill_avg_for_every_skill(self,name):
         df = await self.get_skill_df(name)
@@ -426,7 +423,6 @@ class Stat(commands.Cog, name="stat"):
             data_max_slayer.append(tmp_df)
         await asyncio.sleep(0)
         return data_max_slayer
-
 
 
 
